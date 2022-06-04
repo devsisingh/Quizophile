4d9ce9c7732add9b208ff6fbaa8bdf33673523ac
DEVSI SINGH <grace.emily876@gmail.com>
2022-06-04 07:34:39 +0000

    color change

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -47,7 +47,7 @@ const Question = ({
 
   return (
     <div className="question">
-      <h1>Question {currQues + 1} :</h1>
+      <h1 style={{color:'black'}}>Question {currQues + 1} :</h1>
 
       <div className="singleQuestion">
         <h2>{questions[currQues].question}</h2>
